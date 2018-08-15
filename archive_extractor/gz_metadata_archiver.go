a35b3ea7381992a3fc1c04b6253ef3c50760ef8a
chen <chenk@jfrog.com>
2018-08-15 17:00:48 +0300

    add gz metadata archive extractor

---
+++ archive_extractor/gz_metadata_archiver.go
@@ -0,0 +1,30 @@
+package archive_extractor
+
+import (
+	"github.com/chen-keinan/go-archive-extractor/archive_extractor/archiver_errors"
+	"jfrog.com/xray/compression"
+	"os"
+	"time"
+)
+
+type GzMetadataArchiver struct {
+}
+
+func (ga GzMetadataArchiver) ExtractArchive(path string, processingFunc func(header *ArchiveHeader, params map[string]interface{}) error,
+	params map[string]interface{}) error {
+	archiveFile, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer archiveFile.Close()
+	rc, err := compression.CreateCompression(path).GetReader(archiveFile)
+	if err != nil {
+		return archiver_errors.New(err)
+	}
+	archiveHeader := NewArchiveHeader(rc, "metadata", time.Now().Unix(), 0)
+	err = processingFunc(archiveHeader, params)
+	if err != nil {
+		return err
+	}
+	return nil
+}
